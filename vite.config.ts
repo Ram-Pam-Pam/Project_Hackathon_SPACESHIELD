631d65be3317ccb8379be7c0e52750134634cb70
SMYRDACK <smyrak.milosz@gmail.com>
2026-05-23 21:47:55 +0200

    lekka poprawka pod połączenie w renderze

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig} from 'vite';
+import { fileURLToPath, URL } from 'node:url'; // <-- Zmiana tutaj
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        // Zamiast __dirname, używamy import.meta.url
+        '@': fileURLToPath(new URL('./', import.meta.url)),
       },
     },
+
     server: {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
