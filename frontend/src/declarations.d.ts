2c7cb74c43d97fa07ef9c0bc63305382b60e6231
WegrzynA <agnieszka.wegrzyn67@gmail.com>
2026-05-24 08:43:54 +0200

    poprawka dla rendera

--- frontend/src/declarations.d.ts
+++ frontend/src/declarations.d.ts
@@ -2,3 +2,12 @@ declare module "*.geojson" {
   const value: any;
   export default value;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL?: string;
+  readonly [key: string]: string | boolean | undefined;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
